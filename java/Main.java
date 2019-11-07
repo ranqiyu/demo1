744664eb60fc88e6c98027c80a1f7e7c93f63bb2
qiyu.ran <qiyu.ran@dmall.com>
2019-11-07 13:49:29 +0800

    优化代码1

--- java/Main.java
+++ java/Main.java
@@ -4,6 +4,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("helloworld!");
+        System.out.println("helloworld，这里也修改一下!");
+        System.out.println("我增加了一行代码。");
     }
 }
