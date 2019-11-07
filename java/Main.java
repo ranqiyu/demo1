8c8a1a75875287461c49b700e0f427aad8d4cfd6
qiyu.ran <qiyu.ran@dmall.com>
2019-11-07 13:59:38 +0800

    增加功能Speaker

--- java/Main.java
+++ java/Main.java
@@ -6,5 +6,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("helloworld，这里也修改一下!");
         System.out.println("我增加了一行代码。");
+
+        Speaker s = new Speaker();
+        s.speak();
     }
 }
---
+++ java/Speaker.java
@@ -0,0 +1,9 @@
+/**
+ * @author qiyu.ran
+ * @date 2019/11/7 13:57
+ */
+public class Speaker {
+    public void speak(){
+        System.out.println("speak");
+    }
+}
