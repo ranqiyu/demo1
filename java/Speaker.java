9280a950e2dc5f468164ff76b81a58e44595ce90
qiyu.ran <qiyu.ran@dmall.com>
2019-11-07 14:26:30 +0800

    修改Speaker的Bug

--- java/Speaker.java
+++ java/Speaker.java
@@ -4,6 +4,7 @@
  */
 public class Speaker {
     public void speak(){
-        System.out.println("speak");
+        System.out.println("speak, 在这里修改了Bug");
+        System.out.println("Bug已修改");
     }
 }
